5d4f861ca6cb7485723177dc59ae8325254b2eb8
PerfectCheik <pertork1@umbc.edu>
2020-05-03 19:28:17 +0000

    Try 2

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 
-# Cheik Kone and Perfect Tokornoo Gradebook Application
+# Cheik Kone and Perfect Torkornoo Gradebook Application
 
 
 from flask import Flask, redirect, render_template, request, url_for
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
-from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -31,7 +31,7 @@ login_manager.init_app(app)
 class User(UserMixin, db.Model):
 
 
-    __tablename__ = "intructor"
+    __tablename__ = "intructors"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128))
@@ -55,7 +55,8 @@ def load_user(user_id):
 #    __tablename__ = "students"
 
 #    id = db.Column(db.Integer, primary_key=True)
-#    content = db.Column(db.String(4096))
+#    firstname = db.Column(db.String(128))
+#    lastname = db.Column(db.String(128))
 #    posted = db.Column(db.DateTime, default=datetime.now)
 #    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
 #    commenter = db.relationship('User', foreign_keys=commenter_id)
@@ -64,7 +65,7 @@ def load_user(user_id):
 def index():
     if request.method == "GET":
         #return render_template("main_page.html", comments=Comment.query.all())
-        return 'Hello from Flask!'
+        return render_template("gradebook_main_page.html")
     if not current_user.is_authenticated:
         return redirect(url_for('index'))
     #comment = Comment(content=request.form["contents"], commenter=current_user)
@@ -76,20 +77,16 @@ def index():
 @app.route("/login/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
-        return render_template("login_page.html", error=False)
+        return render_template("gradebook_login_page.html", error=False)
 
-    if request.form["username"] != "admin" or request.form["password"] != "secret":
-        return render_template("login_page.html", error=True)
+    user = load_user(request.form["username"])
+    if user is None:
+        return render_template("gradebook_login_page.html", error=True)
 
+    if not user.check_password(request.form["password"]):
+        return render_template("gradebook_login_page.html", error=True)
 
-#    user = load_user(request.form["username"])
-#   if user is None:
-#        return render_template("login_page.html", error=True)
-
-#    if not user.check_password(request.form["password"]):
-#        return render_template("login_page.html", error=True)
-
-#    login_user(user)
+    login_user(user)
     return redirect(url_for('index'))
 
 @app.route("/logout/")
@@ -97,3 +94,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+#for testing template format and code, route will be deleted
+@app.route("/gradebook/")
+def gradebook():
+    return render_template("gradebook_main_page.html")
