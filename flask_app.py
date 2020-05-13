63870e72a9c5ff402e1abb4c5d924df9a16373b5
PerfectCheik <pertork1@umbc.edu>
2020-05-13 23:33:38 +0000

    Added Gradebook changes

--- flask_app.py
+++ flask_app.py
@@ -77,8 +77,10 @@ class Student(db.Model):
     firstname = db.Column(db.String(128))
     lastname = db.Column(db.String(128))
     student_major = db.Column(db.String(128))
+    student_email = db.Column(db.String(128))
     course_id = db.Column(db.Integer, db.ForeignKey('courses.course_id'), nullable=True)
     course = db.relationship('Course', foreign_keys=course_id)
+    #student email address missing from table
 
 class Gradebook(db.Model):
 
@@ -97,16 +99,63 @@ class Gradebook(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
+        #return render_template("gradebook_main_page.html", grades=Gradebook.query.join(Assignment, Assignment.assignment_id==Gradebook.assignment_id))
+        return render_template("gradebook_main_page.html", grades=db.session.query(Gradebook, Course, Student, Assignment).join(Course, Gradebook.course_id == Course.course_id).join(Student, Gradebook.student_id == Student.student_id).join(Assignment, Gradebook.assignment_id == Assignment.assignment_id))
+  # if request.method == "POST":
+        #new_student=Student(firstname=request.form["first_name"],lastname=request.form["last_name"],student_major=request.form["major"], student_email=request.form["email"],course_id=course_default)
+       # db.session.add(new_student)
+        #db.session.commit()
+    #return render_template ("gradebook_student_roster_page.html", students=Student.query.all())
+
+
+   # if request.method == "GET":
         #return render_template("main_page.html", comments=Comment.query.all())
-        return render_template("gradebook_main_page.html")
-    if not current_user.is_authenticated:
-        return redirect(url_for('index'))
+      #  return render_template("gradebook_main_page.html")
+   # if not current_user.is_authenticated:
+    #    return redirect(url_for('index'))
     #comment = Comment(content=request.form["contents"], commenter=current_user)
     #db.session.add(comment)
     #db.session.commit()
 
-    return redirect(url_for('index'))
 
+
+#default course id for new students
+course_default = 101
+
+#Webpage display student roster in aphlabetical order, redirects user to add and delete students
+@app.route("/student_roster", methods=["GET", "POST"])
+def roster():
+    if request.method == "GET":
+        return render_template("gradebook_student_roster_page.html", students=Student.query.order_by(Student.lastname, Student.firstname).all())
+    if request.method == "POST":
+        new_student=Student(firstname=request.form["first_name"],lastname=request.form["last_name"],student_major=request.form["major"], student_email=request.form["email"],course_id=course_default)
+        db.session.add(new_student)
+        db.session.commit()
+    return render_template ("gradebook_student_roster_page.html", students=Student.query.order_by(Student.lastname, Student.firstname).all())
+
+#Deletes student from database
+@app.route("/<int:id>/student_delete", methods=["POST"])
+def deleteStudent(id):
+    deleted_student = Student.query.get_or_404(id)
+    db.session.delete(deleted_student)
+    db.session.commit()
+    return redirect(url_for('roster'))
+
+#update feature
+@app.route("/update/", methods=["POST"])
+def update_grade():
+    if request.method == "POST":
+        # query.get() method gets a Row by the primary_key
+        record = Student.query.get(request.form.get('id'))
+        # change the values you want to update
+        record.student_major = request.form.get('change_grade')
+        # commit changes
+
+        db.session.commit()
+    # redirect back to your main view
+    return redirect(url_for('roster'))
+
+#login page
 @app.route("/login/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -132,3 +181,54 @@ def logout():
 @app.route("/gradebook/")
 def gradebook():
     return render_template("gradebook_main_page.html")
+
+#for Adding assignment records, route will be updated
+@app.route("/assignments/")
+def addAssigment():
+#    assignment1 = Assignment(assignment_desc='Homework 1',course_id=course_default)
+#    db.session.add(assignment1)
+#    assignment2 = Assignment(assignment_desc='Homework 2',course_id=course_default)
+#    db.session.add(assignment2)
+#    assignment3 = Assignment(assignment_desc='Term Paper',course_id=course_default)
+#    db.session.add(assignment3)
+#    assignment4 = Assignment(assignment_desc='Final Exam',course_id=course_default)
+#    db.session.add(assignment4)
+#    db.session.commit()
+    return 'Assignments 1-4 Added'
+
+@app.route("/add-gradebooks/")
+def addGrade():
+#    grade1 = Gradebook(course_id=course_default, student_id = 1 , assignment_id = 1 , grade = 76 )
+#    db.session.add(grade1)
+#    grade2 = Gradebook(course_id=course_default, student_id = 2 , assignment_id = 1 , grade = 88 )
+#    db.session.add(grade2)
+#    grade3 = Gradebook(course_id=course_default, student_id = 3 , assignment_id = 1 , grade = 79 )
+#    db.session.add(grade3)
+#    grade4 = Gradebook(course_id=course_default, student_id = 4 , assignment_id = 1 , grade = 100 )
+#    db.session.add(grade4)
+#    grade5 = Gradebook(course_id=course_default, student_id = 3 , assignment_id = 2 , grade = 99 )
+#    db.session.add(grade5)
+#    grade6 = Gradebook(course_id=course_default, student_id = 4 , assignment_id = 2 , grade = 75 )
+#    db.session.add(grade6)
+#    grade7 = Gradebook(course_id=course_default, student_id = 2 , assignment_id = 2 , grade = 85 )
+#    db.session.add(grade7)
+#    grade8 = Gradebook(course_id=course_default, student_id = 1 , assignment_id = 2 , grade = 100 )
+#    db.session.add(grade8)
+#    grade9 = Gradebook(course_id=course_default, student_id = 2 , assignment_id = 3 , grade = 87 )
+#    db.session.add(grade9)
+#    grade10 = Gradebook(course_id=course_default, student_id = 4 , assignment_id = 3 , grade = 95 )
+#    db.session.add(grade10)
+#    grade11 = Gradebook(course_id=course_default, student_id = 3 , assignment_id = 3 , grade = 70 )
+#    db.session.add(grade11)
+#    grade12 = Gradebook(course_id=course_default, student_id = 1 , assignment_id = 3 , grade = 80 )
+#    db.session.add(grade12)
+#    grade13 = Gradebook(course_id=course_default, student_id = 1 , assignment_id = 4 , grade = 97 )
+#    db.session.add(grade13)
+#    grade14 = Gradebook(course_id=course_default, student_id = 2 , assignment_id = 4 , grade = 100 )
+#    db.session.add(grade14)
+#    grade15 = Gradebook(course_id=course_default, student_id = 3 , assignment_id = 4 , grade = 75 )
+#    db.session.add(grade15)
+#    grade16 = Gradebook(course_id=course_default, student_id = 4 , assignment_id = 4 , grade = 78 )
+#    db.session.add(grade16)
+#    db.session.commit()
+    return 'Grades 1-16 Added'
